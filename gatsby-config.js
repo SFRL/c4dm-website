ff73dcef5595b9b113c24853fc18bf8da88e2644
teo523 <teodoro.dan@gmail.com>
2023-08-04 10:45:10 +0100

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
             embedURL: (videoId) =>
               `https://www.youtube-nocookie.com/embed/${videoId}`,
           },
-        ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+        ], //Optional : Override URL of a service provider, e.g to enable youtube-nocookie support
         containerClass: "embedVideo-container", //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
         iframeId: false, //Optional: if true, iframe's id will be set to what is provided after 'video:' (YouTube IFrame player API requires iframe id)
       },
